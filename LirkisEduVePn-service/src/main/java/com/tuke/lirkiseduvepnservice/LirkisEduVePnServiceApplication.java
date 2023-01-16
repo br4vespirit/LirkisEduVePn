b0591e3beed8d97e1cdabfe0ad318b595ca70d9d
Dmytro Demianenko <br4vespirit@gmail.com>
2023-01-16 17:34:23 +0100

    initialized project

---
+++ LirkisEduVePn-service/src/main/java/com/tuke/lirkiseduvepnservice/LirkisEduVePnServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tuke.lirkiseduvepnservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LirkisEduVePnServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LirkisEduVePnServiceApplication.class, args);
+    }
+
+}
---
+++ LirkisEduVePn-service/src/test/java/com/tuke/lirkiseduvepnservice/LirkisEduVePnServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuke.lirkiseduvepnservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LirkisEduVePnServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
