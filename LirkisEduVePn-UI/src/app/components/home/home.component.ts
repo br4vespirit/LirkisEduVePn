ede3979998c9448ac02245785c7f9131f2002501
Lukas Pisarcik <lukas.pisarcik@student.tuke.sk>
2023-01-29 00:24:39 +0100

    home page done without modals

--- LirkisEduVePn-UI/src/app/components/home/home.component.ts
+++ LirkisEduVePn-UI/src/app/components/home/home.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
-import {UtilsService} from "../../services/utils.service";
+import {UserProfile} from "../../models/user-profile.model";
+import {Subscription} from "rxjs";
+import {BackendService} from "../../services/backend.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  token: string = this._utils.getToken();
 
-  constructor(private _utils: UtilsService){}
+export class HomeComponent {
+ // @ts-ignore
+ profile: UserProfile = '';
+ profileSubscription: Subscription = new Subscription();
+
+ constructor(private _client: BackendService) {}
+
+ ngOnInit(): void {
+   this.profileSubscription = this._client.fetchProfile().subscribe(data => {
+     this.profile = data as UserProfile;
+   })
+ }
+
+ ngOnDestroy(): void {
+   this.profileSubscription.unsubscribe();
+ }
 }
--- LirkisEduVePn-UI/src/app/components/navbar/navbar.component.ts
+++ LirkisEduVePn-UI/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,31 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {UserProfile} from "../../models/user-profile.model";
+import {Subscription} from "rxjs";
+import {BackendService} from "../../services/backend.service";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy{
   colapse: boolean = false
+   // @ts-ignore
+  profile: UserProfile;
+  profileSubscription: Subscription = new Subscription();
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private _client: BackendService) {
+  }
+
+  ngOnInit(): void {
+    this.profileSubscription = this._client.fetchProfile().subscribe(data => {
+      this.profile = data as UserProfile;
+    })
+  }
+ 
+  ngOnDestroy(): void {
+    this.profileSubscription.unsubscribe();
   }
 
   showMenu() {
@@ -21,6 +37,7 @@ export class NavbarComponent {
   }
 
   logout() {
+    this.colapse = !this.colapse
     localStorage.removeItem("jwt-token");
     this.router.navigate(["/login"]).then(r => r);
   }
