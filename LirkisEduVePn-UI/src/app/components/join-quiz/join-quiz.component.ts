30d5fa88c925dce539acb07cafa56e9c80e83d61
Lukas Pisarcik <lukas.pisarcik@student.tuke.sk>
2023-01-30 00:46:51 +0100

    added redirect to join quiz modal

--- LirkisEduVePn-UI/src/app/components/join-quiz/join-quiz.component.ts
+++ LirkisEduVePn-UI/src/app/components/join-quiz/join-quiz.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { SCENE_URL } from "../../constants";
 
 @Component({
   selector: 'app-join-quiz',
@@ -11,6 +12,10 @@ export class JoinQuizComponent {
 
   constructor(private matDialogRef:MatDialogRef<JoinQuizComponent>){}
 
+  openScene(){
+    // @ts-ignore
+    window.location.href= SCENE_URL;
+  }
   closeDialog(){
     this.matDialogRef.close();
   }
