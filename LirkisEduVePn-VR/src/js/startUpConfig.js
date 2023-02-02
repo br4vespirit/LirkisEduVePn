af8f99f8776a160b5afd57307a11fe5b7e53bff7
Lukas Pisarcik <lukas.pisarcik@student.tuke.sk>
2023-02-02 15:47:08 +0100

    same values added as classes

--- LirkisEduVePn-VR/src/js/startUpConfig.js
+++ LirkisEduVePn-VR/src/js/startUpConfig.js
@@ -3,13 +3,19 @@ const getJsonData = (url) =>{
     fetch(url).then((res) => {
          res.json().then((data)=>{
              for (const key in data){
-                 const text = document.getElementById(key);
+                 const text = document.getElementById(key) !== null ? document.getElementById(key) : document.getElementsByClassName(key); 
                  if (text){
-                     text.setAttribute('value', data[key]);
+                    if(text.length){
+                        for (let i=0; i<text.length; i++) {
+                            text[i].setAttribute('value', data[key]);
+                        }
+                    }else {
+                        text.setAttribute('value', data[key]);
+                    }
                  }
-             }
+            }
         });
-    })
+    });
 }
 
 getJsonData('../assets/sceneDescription.json');
