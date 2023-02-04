bd8adf37852e3a6dacf6086591b3d26b5b5feffd
Lukas Pisarcik <lukas.pisarcik@student.tuke.sk>
2023-02-04 16:39:01 +0100

    error handling

--- LirkisEduVePn-VR/src/js/startUpConfig.js
+++ LirkisEduVePn-VR/src/js/startUpConfig.js
@@ -1,21 +1,25 @@
 // change description in the scene
-const getJsonData = (url) =>{
-    fetch(url).then((res) => {
-         res.json().then((data)=>{
-             for (const key in data){
-                 const text = document.getElementById(key) !== null ? document.getElementById(key) : document.getElementsByClassName(key); 
-                 if (text){
-                    if(text.length){
-                        for (let i=0; i<text.length; i++) {
-                            text[i].setAttribute('value', data[key]);
-                        }
-                    }else {
-                        text.setAttribute('value', data[key]);
+const getJsonData = (url) => {
+    try{
+        fetch(url).then((res) => {
+            res.json().then((data)=>{
+                for (const key in data){
+                    const text = document.getElementById(key) !== null ? document.getElementById(key) : document.getElementsByClassName(key); 
+                    if (text){
+                       if(text.length){
+                           for (let i=0; i<text.length; i++) {
+                               text[i].setAttribute('value', data[key]);
+                           }
+                       }else {
+                           text.setAttribute('value', data[key]);
+                       }
                     }
-                 }
-            }
-        });
-    });
+               }
+           });
+       });
+    }catch(error){
+        console.error(error);
+    }
 }
 
 getJsonData('../assets/sceneDescription.json');
