a2d36a71905ecde311c96dc2b081502ece6a1bb1
Lukas Pisarcik <lukas.pisarcik@student.tuke.sk>
2023-03-09 16:06:33 +0100

    info panels fixed

--- LirkisEduVePn-UI/src/app/components/scene/scene.component.ts
+++ LirkisEduVePn-UI/src/app/components/scene/scene.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import './js/components/collisionDetectorEventHandler.component.js';
 import './js/components/toggleInfo.component.js';
@@ -12,5 +12,14 @@ import './js/components/petriNetSim.component.js';
   templateUrl: './scene.component.html',
   styleUrls: ['./scene.component.css']
 })
-export class SceneComponent {
+export class SceneComponent implements OnInit{
+  ngOnInit(): void {
+
+    // TODO: get language files from database
+
+    // TODO: get pnml file from database
+
+    // TODO: get scene file form database (html file)
+
+  }
 }
