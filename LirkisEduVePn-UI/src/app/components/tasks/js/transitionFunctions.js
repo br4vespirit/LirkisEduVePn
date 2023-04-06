3427a000263b6bad4cdb72344dd3a742ea0d41fd
Lukas Pisarcik <lukas.pisarcik@student.tuke.sk>
2023-04-06 22:00:15 +0200

    confirm buttons updated

--- LirkisEduVePn-UI/src/app/components/tasks/js/transitionFunctions.js
+++ LirkisEduVePn-UI/src/app/components/tasks/js/transitionFunctions.js
@@ -1,37 +1,17 @@
 
 export const playConfirmationSuccessSound = (elementId) => {
-    if (elementId.includes('confirm')) {
-      var confirmationEntity = document.querySelector(`#${elementId}`);
-      confirmationEntity.setAttribute('material', 'color: #36c991;');
-      confirmationEntity.setAttribute('text', 'value: Správna odpoveď');
-      confirmationEntity.emit('success');
-    }
+  const confirmationEntity = document.getElementById(elementId);
+  confirmationEntity.setAttribute('material', 'color: #36c991;');
+  confirmationEntity.setAttribute('text', 'value: Správna odpoveď');
+  confirmationEntity.emit('success');
 }
 
 export const playConfirmationUnsuccessSound = (elementId) => {
-    if (elementId.includes('confirm')) {
-      var confirmationEntity = document.querySelector(`#${elementId}`);
-      confirmationEntity.emit('unsuccess');
-    }
+  const confirmationEntity = document.getElementById(elementId);
+  confirmationEntity.emit('unsuccess');
 }
 
 export const updateVisualProgres = (elementId) =>  {
-    if (elementId.includes('confirm')) {
-      var progressEntity = document.querySelector(`#${elementId}Progress`);
-      progressEntity.setAttribute(
-        'material',
-        'color: green; side: double; shader: flat'
-      );
-    }
-}
-
-export const changeSelectButton = (elementId) => {
-  const clkMultiEventHandler = elementId.getAttribute('clk-multi-event-handler').selected;
-  if (clkMultiEventHandler === false){
-    elementId.setAttribute('material', 'src: #selected; side: double; shader: flat');
-  }else{
-    elementId.setAttribute('material', 'src: #unselected; side: double; shader: flat');
-  }
-
-  elementId.setAttribute('clk-multi-event-handler', `selected: ${!clkMultiEventHandler}`);
+  const progressEntity = document.getElementById(elementId);
+  progressEntity.setAttribute('material', 'color: green; side: double; shader: flat');
 }
